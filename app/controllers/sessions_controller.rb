e4ba989cbdfc70095c4b7cb87f3ed6db576d13a2
dylan <dyan2009200@gmail.com>
2022-08-17 03:35:18 +0800

    WI sign_in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,15 @@
 class SessionsController < ApplicationController
   def create
-    hashed_password = Digest::SHA1.hexdigest("xx-#{params[:user][:password]}yy")
-
-    user = User.find_by(
-      email: params[:user][:email],
-      password: hashed_password
-    )
+    # render html: params[:user][:email]
+    user = User.login(params[:user])
     if user
+      # 發號碼牌 發session => rails controller內建的東西，結構像hash
+      #[:可以自己填自己喜歡的名字]
+      session[:user_session] = user.id
+
       redirect_to "/", notice: "登入成功"      
     else
-      redirect_to "/users/sign_up"
+      redirect_to "/users/sign_in", notice: "登入失敗"
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,18 @@ class User < ApplicationRecord
   # password 
   validates :password, presence: true, length: {minimum: 6}, confirmation: true
 
+  # 登入判斷
+  # ruby 3 可以這樣寫
+  #def self.login(email:, password:)
+  def self.login(user_params)
+    email = user_params[:email]
+    password = user_params[:password]
+
+    hashed_password = Digest::SHA1.hexdigest("xx-#{password}yy")
+    find_by(email: email, password: hashed_password)
+  end
+
+
   # callback
   # 密碼存進資料庫前要做的事情,建立之前先加鹽
   before_create :encrypt_password
